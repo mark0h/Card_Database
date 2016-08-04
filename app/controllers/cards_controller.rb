525c30211524aeb9f99aea4feb283d04a6191cbc
Marko <markogardner@gmail.com>
2016-08-04 16:24:29 -0700

    Got nested form to work, looks good. Working on getting the params to pass to controller - work in progress

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -22,13 +22,16 @@ class CardsController < ApplicationController
   end
 
   def create
+    flash[:success] = "----params: #{params}\ncard_params: #{card_params}\ncard_traits_params: #{card_traits_params}"
+    render 'index'
+
     @card = Card.new(card_params)
-    if @card.save
-      flash[:success] = "#{@card.name} card successfully created"
-      redirect_to cards_path
-    else
-      render 'new'
-    end
+    # if @card.save
+    #   flash[:success] = "#{@card.name} card successfully created"
+    #   redirect_to cards_path
+    # else
+    #   render 'new'
+    # end
   end
 
   def show
@@ -46,6 +49,10 @@ class CardsController < ApplicationController
   private
   def card_params
     params.require(:card).permit(:name, :type_id)
+  end
+
+  def card_traits_params
+    params.require(:card_traits).permit(:trait_id, :card_id, :value)
   end
 
 end
